638e1fe1977eb9e04abbef402feefae4890347f1
Tulajaram12 <96332761+Tulajaram12@users.noreply.github.com>
2026-09-02 15:28:44 +0530

    Create Home.jsx

---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+function Home() {
+  return (
+    <div className="page">
+      <h2>Welcome to My E-Commerce Application</h2>
+
+      <p>
+        This is a simple React frontend application deployed on Amazon EKS.
+      </p>
+
+      <p>
+        Frontend → Backend Microservices → Database
+      </p>
+    </div>
+  );
+}
+
+export default Home;
